ff48b3ffbef19e5d6bb1e2ecf4c23f010230d3b5
yanesewailing <user@users-MacBook-Air.local>
2022-11-30 11:42:48 +0600

    .

---
+++ ugolek/src/App.js
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar/Navbar";
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import MainRoutes from "./Routes/MainRoutes";
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <MainRoutes />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
